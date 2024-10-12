0c55b52f5d220685a22e917e2796ae9c861d1a0b
lue-bird <lue.the.bird@gmail.com>
2024-10-12 05:32:50 +0200

    update setup

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,36 @@
-import { defineConfig } from 'vite'
-import elm from 'vite-plugin-elm-watch'
+import { defineConfig } from "vite"
+import elm from "vite-plugin-elm-watch"
+import * as elmOptimizeLevel2 from "elm-optimize-level-2"
+import * as path from "node:path"
 
-export default defineConfig({
-    plugins: [elm()]
-})
+export default defineConfig((environment) => ({
+    plugins: [
+        elm({
+            mode:
+                environment.command === "build" ?
+                    "optimize"
+                    : "debug"
+
+        }),
+        environment.command === "build" ?
+            applyEOL2()
+            : []
+    ],
+    base: "/lider/"
+}))
+
+const jsFileRegex = /\.(js)$/
+function applyEOL2() {
+    return {
+        name: 'apply-elm-optimize-level-2',
+
+        async transform(src, id) {
+            if (jsFileRegex.test(id)) {
+                return {
+                    code: await elmOptimizeLevel2.transform(src, true),
+                    map: null
+                }
+            }
+        }
+    }
+}
